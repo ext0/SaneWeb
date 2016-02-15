4a50ed444630d77a5fc0188f8351e40276f8372a
Patrick Bell <widoreu.bell@gmail.com>
2016-02-15 11:27:36 -0600

    Implemented model abstraction

---
+++ ArmWebInterface/Data/Model.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using SaneWeb.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaneWeb.Data
+{
+    [Serializable()]
+    public abstract class Model<T> where T : Model<T>
+    {
+        private int id;
+
+        public Model() //generate UNIQUE ID
+        {
+            String db = DBReferences.findDBStoring<T>();
+            int id = new Random().Next(0, int.MaxValue);
+            while (!DBReferences.checkIdUnique<T>(db, id)) { }
+            this.id = id;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public static T deepClone(T source)
+        {
+            String serialized = JsonConvert.SerializeObject(source);
+            T obj = JsonConvert.DeserializeObject<T>(serialized);
+            obj.id = source.id;
+            return JsonConvert.DeserializeObject<T>(serialized);
+        }
+    }
+}
